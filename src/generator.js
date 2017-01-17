bda302d8c832efd56c468c3e7d39490dac213658
Józef Marek <ementos@gmail.com>
2017-01-17 23:20:12 +0100

    Fixed generator to generate permutations of given features

--- src/generator.js
+++ src/generator.js
@@ -14,8 +14,41 @@ import _ from 'lodash';
 export default function generate(mixture, nrOfPeople) {
     let variants = generateVariants(mixture);
     let val = values(variants);
-    let allProfiles = allPossibleNElementArrays(val, nrOfPeople);
-    let flat = slice(uniqWith(flattenDeep(allProfiles), isEqual), 0, nrOfPeople);
+    let allProfiles = getAllPermutations(val, nrOfPeople);
+
+    let flat = allProfiles;
     console.log('v', val, '->', flat);
     return flat;
+}
+
+export function getAllPermutations(val, nrOfPeople) {
+    let generatedFeatures = val[0];
+    let stopGenerating = false;
+    for(let i = 1; i < val.length; i++) {
+        let lastLength = generatedFeatures.length;
+        for (let j = 0; j < lastLength; j++) {
+            let featureToJoin = generatedFeatures[j]
+            for (let k = 0; k < val[i].length; k++) {
+                let joinedElement = featureToJoin + val[i][k];
+                if (k == 0)
+                    generatedFeatures[j] = joinedElement;
+                else if (generatedFeatures.length < nrOfPeople)
+                    generatedFeatures.push(joinedElement);
+                else {
+                    stopGenerating = true;
+                    break;
+                }
+            }
+
+            if (stopGenerating)
+                break;
+        }
+
+        if (stopGenerating)
+            break;
+    }
+
+    if (generatedFeatures.length > nrOfPeople)
+        return generatedFeatures.slice(0, nrOfPeople);
+    return generatedFeatures;
 }
