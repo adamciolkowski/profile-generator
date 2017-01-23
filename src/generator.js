1fe9a846488fecca078f03f46428f13eedee8cbd
Adam Ciołkowski <adamciolkowski1@gmail.com>
2017-01-23 22:06:43 +0100

    Removed unused imports

--- src/generator.js
+++ src/generator.js
@@ -1,16 +1,5 @@
-import allPossiblePairs from './allPossiblePairs';
-import {allPossibleNElementArrays} from './allPossiblePairs';
-import groupByCount from './groupByCount';
-import containsAll from './containsAll';
-import flattenDeep from 'lodash/flattenDeep';
-import isEqual from 'lodash/isEqual';
-import slice from 'lodash/slice';
-import chunk from 'lodash/chunk';
-import sortBy from 'lodash/sortBy';
-import uniqWith from 'lodash/uniqWith';
 import values from 'lodash/values';
 import generateVariants from './variantsGenerator';
-import _ from 'lodash';
 
 export default function generate(mixture, nrOfPeople) {
     let variants = generateVariants(mixture);
